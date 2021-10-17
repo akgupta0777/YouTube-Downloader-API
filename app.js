fea556fde983062e6a5baf85d99ec21f9bbe33ad
akgupta0777 <akgupta0777@gmail.com>
2021-10-17 15:10:00 +0530

    made video info route

--- app.js
+++ app.js
@@ -4,21 +4,20 @@ const ytdl = require('ytdl-core');
 const app =  express();
 app.use(cors());
 
-const getmeinfo = async (URL) => {
-    console.log("Getting Info ...");
-    const info = await ytdl.getInfo(URL);
-    console.log(info);
-}
-
 app.get('/',(req,res) => {
     res.send('Hello world');
     console.log("Hey on port 3000")
 })
 
+app.get('/getVideoInfo',async (req,res) => {
+    const URL = req.query.URL;
+    const info = await ytdl.getInfo(URL);
+    res.send(info.formats);
+})
+
 app.get('/download',(req,res) => {
     const URL = req.query.URL;
     console.log(URL);
-    getmeinfo(URL);
 
     res.header('Content-Disposition', 'attachment; filename="video.mp4"');
     ytdl(URL, {

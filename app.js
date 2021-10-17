95bc671e5e98f848aea3b1d79c101d1cce574496
akgupta0777 <akgupta0777@gmail.com>
2021-10-17 13:26:18 +0530

    Modified a little bit to stream 1080p videos

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const ytdl = require('ytdl-core');
 const app =  express();
 app.use(cors());
 
+const getmeinfo = async (URL) => {
+    console.log("Getting Info ...");
+    const info = await ytdl.getInfo(URL);
+    console.log(info);
+}
+
 app.get('/',(req,res) => {
     res.send('Hello world');
     console.log("Hey on port 3000")
@@ -12,10 +18,11 @@ app.get('/',(req,res) => {
 app.get('/download',(req,res) => {
     const URL = req.query.URL;
     console.log(URL);
+    getmeinfo(URL);
 
     res.header('Content-Disposition', 'attachment; filename="video.mp4"');
     ytdl(URL, {
-        format: 'mp4'
+        quality:137,
     }).pipe(res);
 })
 

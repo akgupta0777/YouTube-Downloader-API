d787deb99a2d41b1163e748b252d61b231392595
akgupta0777 <akgupta0777@gmail.com>
2021-10-18 15:02:03 +0530

    Used Web Scraping Approach

---
+++ Engine.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const puppeteer = require('puppeteer');
+const app =  express();
+app.use(cors());
+
+const getAvailableFormats = async (videoID) => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage();
+  await page.goto(`https://www.y2mate.com/youtube/${videoID}`);
+  await page.waitForSelector('td');
+  const elements = await page.evaluate(() => Array.from(document.querySelectorAll("td"),e => e.innerText));
+  const formats = [];
+  for(let i=0;i<elements.length;i+=3){
+    formats.push({
+      "innerText":elements[i].trim(),
+      "size":elements[i+1].trim(),
+      "index":(i/3),
+    });
+  }
+  await browser.close();
+  return formats;
+}
+
+const getVideoLink =  async (videoID,index) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`https://www.y2mate.com/youtube/${videoID}`,{
+    waitUntil:'networkidle0'
+  });
+  let btns = await page.$$("a[type=button]");
+  await btns[index].click();
+  await page.waitForSelector('.btn.btn-success.btn-file');
+  const videoLink = await page.$eval('a.btn.btn-success.btn-file',e => e.href);
+  await browser.close();
+  return videoLink;
+}
+
+app.get('/download',async (req,res) => {
+    const videoID = req.query.videoID;
+    const index = req.query.index;
+    const videoLink = await getVideoLink(videoID,index);
+    res.redirect(videoLink);
+})
+
+app.get('/getAvailableFormats',async (req,res) => {
+  const videoID = req.query.videoID;
+  const formats = await getAvailableFormats(videoID);
+  res.send(formats);
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port,async () => {
+    console.log(`Green Lights! Server is up and running on port ${port}`);
+})
--- app.js
+++ app.js
@@ -1,31 +1,145 @@
-const express = require('express');
-const cors = require('cors');
-const ytdl = require('ytdl-core');
-const app =  express();
-app.use(cors());
+/* THIS FILE IS MARKED AS DEPRECATED
+ * 
+ * The approach used here uses a streaming library ytdl for nodeJS servers.
+ * The library streams youtube videos and process them and we pipe them 
+ * through response to initiate download.
+ * 
+ * THE PROBLEM WITH THIS APPROACH
+ * Typically 480p, 720p, 1080p or better videos do not have audio encoded with it.
+ * The audio must be downloaded separately and merged via an encoding library. 
+ * ffmpeg is the most widely used tool, with many Node.js modules available. 
+ * Use the format objects returned from ytdl.getInfo to download specific 
+ * streams to combine to fit your needs.
+ * 
+ * WHAT IS THE CURRENT SOLUTION
+ * We have actually created a scraping Engine using Puppeteer a headless chrome
+ * automation library for server needs. Our engine scrapes videos from y2mate.com
+ * and redirects the user to download URL.
+ */
+// const express = require('express');
+// const cors = require('cors');
+// const ytdl = require('ytdl-core');
+// const ffmpeg = require('ffmpeg-static');
+// const cp = require('child_process');
+// const readline = require('readline');
+// const app =  express();
+// app.use(cors());
 
-app.get('/',(req,res) => {
-    res.send('Hello world');
-    console.log("Hey on port 3000")
-})
+// const mergeVideoAndAudio = (URL,quality) => {
+// const ref = URL;
+// const tracker = {
+//   start: Date.now(),
+//   audio: { downloaded: 0, total: Infinity },
+//   video: { downloaded: 0, total: Infinity },
+//   merged: { frame: 0, speed: '0x', fps: 0 },
+// };
 
-app.get('/getVideoInfo',async (req,res) => {
-    const URL = req.query.URL;
-    const info = await ytdl.getInfo(URL);
-    res.send(info.formats);
-})
+// // Get audio and video streams
+// const audio = ytdl(ref, { quality: 'highestaudio' })
+//   .on('progress', (_, downloaded, total) => {
+//     tracker.audio = { downloaded, total };
+//   });
+// const video = ytdl(ref, { quality: quality })
+//   .on('progress', (_, downloaded, total) => {
+//     tracker.video = { downloaded, total };
+//   });
 
-app.get('/download',(req,res) => {
-    const URL = req.query.URL;
-    console.log(URL);
+// // Prepare the progress bar
+// let progressbarHandle = null;
+// const progressbarInterval = 1000;
+// const showProgress = () => {
+//   readline.cursorTo(process.stdout, 0);
+//   const toMB = i => (i / 1024 / 1024).toFixed(2);
 
-    res.header('Content-Disposition', 'attachment; filename="video.mp4"');
-    ytdl(URL, {
-        quality:137,
-    }).pipe(res);
-})
+//   process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
+//   process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
 
-const port = process.env.PORT || 5000;
-app.listen(port,() => {
-    console.log(`Green Lights! Server is up and running on ${port}`);
-})
+//   process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
+//   process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
+
+//   process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
+//   process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
+
+//   process.stdout.write(`running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
+//   readline.moveCursor(process.stdout, 0, -3);
+// };
+
+// Start the ffmpeg child process
+// const ffmpegProcess = cp.spawn(ffmpeg, [
+//   // Remove ffmpeg's console spamming
+//   '-loglevel', '8', '-hide_banner',
+//   // Redirect/Enable progress messages
+//   '-progress', 'pipe:3',
+//   // Set inputs
+//   '-i', 'pipe:4',
+//   '-i', 'pipe:5',
+//   // Map audio & video from streams
+//   '-map', '0:a',
+//   '-map', '1:v',
+//   // Keep encoding
+//   '-c:v', 'copy',
+//   // Define output file
+//   'out.mp4',
+// ], {
+//   windowsHide: true,
+//   stdio: [
+    /* Standard: stdin, stdout, stderr */
+    // 'inherit', 'inherit', 'inherit',
+    /* Custom: pipe:3, pipe:4, pipe:5 */
+    // 'pipe', 'pipe', 'pipe',
+//   ],
+// }); 
+
+// ffmpegProcess.on('close', () => {
+//   console.log('done');
+//   // Cleanup
+//   process.stdout.write('\n\n\n\n');
+//   clearInterval(progressbarHandle);
+// });
+
+// // Link streams
+// // FFmpeg creates the transformer streams and we just have to insert / read data
+// ffmpegProcess.stdio[3].on('data', chunk => {
+//   // Start the progress bar
+//   if (!progressbarHandle) progressbarHandle = setInterval(showProgress, progressbarInterval);
+//   // Parse the param=value list returned by ffmpeg
+//   const lines = chunk.toString().trim().split('\n');
+//   const args = {};
+//   for (const l of lines) {
+//     const [key, value] = l.split('=');
+//     args[key.trim()] = value.trim();
+//   }
+//   tracker.merged = args;
+// });
+// audio.pipe(ffmpegProcess.stdio[4]);
+// video.pipe(ffmpegProcess.stdio[5]);
+// }
+
+// app.get('/',(req,res) => {
+//     res.send('Hello world');
+//     console.log("Hey on port 5000")
+// })
+
+// app.get('/getVideoInfo',async (req,res) => {
+//     const URL = req.query.URL;
+//     const info = await ytdl.getInfo(URL);
+//     console.log("Formats available ");
+//     console.log(info.formats);
+//     res.send(info.formats);
+//     mergeVideoAndAudio(URL,399);
+// })
+
+// app.get('/download',(req,res) => {
+//     const URL = req.query.URL;
+//     console.log(URL);
+
+//     res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+//     ytdl(URL, {
+//         quality:299,
+//     }).pipe(res);
+// })
+
+// const port = process.env.PORT || 5000;
+// app.listen(port,() => {
+//     console.log(`Green Lights! Server is up and running on ${port}`);
+// })
